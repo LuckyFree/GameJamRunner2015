13e63fdc43e23fc922c9105412a183635497b84f
Kevin Drure <kevin.drure@gmail.com>
2015-11-17 13:57:30 +0100

    AudioButtonSwapper source added
    Can swap 2 audio buttons randomly

---
+++ Assets/Dev/UI/AudioButtonSwapper.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioButtonSwapper : MonoBehaviour
+{
+	#region Attributes
+
+		// Swap translation duration
+		[SerializeField]
+		private float m_SwapTranslationDuration = 1;
+
+	#endregion
+
+	#region MonoBehaviour
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			// Debug
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				Swap();
+			}
+		}
+
+	#endregion
+
+	#region Public Manipulators
+
+		/// <summary>
+		/// Random swap audio buttons
+		/// </summary>
+		public void Swap()
+		{
+			int randA = Random.Range(0, 4);
+			int randB;
+
+			do
+			{
+				randB = Random.Range(0, 4);
+			}
+			while (randB == randA);
+
+			AudioButton buttonA = AudioButton.GetAudioButtonWithID((AudioButton.EButtonID)randA);
+			AudioButton buttonB = AudioButton.GetAudioButtonWithID((AudioButton.EButtonID)randB);
+
+			Swap(buttonA, buttonB);
+		}
+
+	#endregion
+
+	#region Private Manipulators
+
+		/// <summary>
+		/// Swap 2 audio button
+		/// </summary>
+		/// <param name="leftButton"></param>
+		/// <param name="rightButton"></param>
+		private void Swap(AudioButton leftButton, AudioButton rightButton)
+		{
+			/*Vector3 temp = leftButton.transform.position;
+			leftButton.transform.position = rightButton.transform.position;
+			rightButton.transform.position = temp;*/
+
+			StartCoroutine(CR_SwapTranslation(leftButton, rightButton));
+		}
+
+	#endregion
+
+	#region Coroutines
+
+		private IEnumerator CR_SwapTranslation(AudioButton leftButton, AudioButton rightButton)
+		{
+			float t = 0;
+			Vector3 oldLeft = leftButton.transform.position;
+			Vector3 oldRight = rightButton.transform.position;
+
+			while (t < m_SwapTranslationDuration)
+			{
+				t += Time.deltaTime;
+
+				leftButton.transform.position = Vector3.Lerp(oldLeft, oldRight, t / m_SwapTranslationDuration);
+				rightButton.transform.position = Vector3.Lerp(oldRight, oldLeft, t / m_SwapTranslationDuration);
+
+				yield return null;
+			}
+		}
+
+	#endregion
+}
