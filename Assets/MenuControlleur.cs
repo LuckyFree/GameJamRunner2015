afd11d4b79c83cb044310ad3351d320f5d8e549d
Kevin Vacquier <kvacquier@icloud.com>
2015-11-17 17:40:19 +0100

    I Hate Library

---
+++ Assets/MenuControlleur.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuControlleur : MonoBehaviour {
+
+	public void StartGame()
+	{
+		Application.LoadLevel("ProtoA");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
